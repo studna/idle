37ed415f4a1696316ced8064ddc9e03d0ef3f2dc
bugman <onlyforwin@gmail.com>
2019-07-09 22:55:57 +0200

    Updates

---
+++ precache-manifest.b7e65b78276e98591c579542212069b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0303ab79608f7450b58",
+    "url": "/idle/static/js/runtime~main.b0303ab7.js"
+  },
+  {
+    "revision": "56e0e46398a92b51ecf3",
+    "url": "/idle/static/js/main.56e0e463.chunk.js"
+  },
+  {
+    "revision": "575c1c0c85a5e3e5303d",
+    "url": "/idle/static/js/1.575c1c0c.chunk.js"
+  },
+  {
+    "revision": "56e0e46398a92b51ecf3",
+    "url": "/idle/static/css/main.f2f0d75d.chunk.css"
+  },
+  {
+    "revision": "7a7eb5b78ad6a935bcaabc8e7de0162c",
+    "url": "/idle/index.html"
+  }
+];
