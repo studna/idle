cbe5e46b2358e2317310b9d54eb5265383ae8cbb
bugman <onlyforwin@gmail.com>
2019-09-06 19:23:04 +0200

    Updates

---
+++ precache-manifest.935da83db573e00994da6b98b5dcb8e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e406b8ff39f4391daac",
+    "url": "/static/js/main.2e406b8f.chunk.js"
+  },
+  {
+    "revision": "a28adaf553dead70973a",
+    "url": "/static/js/1.a28adaf5.chunk.js"
+  },
+  {
+    "revision": "2e406b8ff39f4391daac",
+    "url": "/static/css/main.177917c5.chunk.css"
+  },
+  {
+    "revision": "bb12a31d2ebf5e51263f2d11489afeef",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.488f94a7f57d98bbc43bae9e6e818de6.js"
+  "/precache-manifest.935da83db573e00994da6b98b5dcb8e5.js"
 );
 
 workbox.clientsClaim();
