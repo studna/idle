d696180a0fe906e3fa5b1bea363e9ad2ca2c523c
Samster <cester.samuele@gmail.com>
2020-02-11 17:05:57 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5155f9b9e2255dd7862980f59f420950.js"
+  "/precache-manifest.4f00f2bcef8bb31934209e74269b9fec.js"
 );
 
 workbox.clientsClaim();
